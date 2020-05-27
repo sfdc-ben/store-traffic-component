268d4b8afd1dfabd07a971c7fc2ffa82629e4556
sfdc-ben <55468295+sfdc-ben@users.noreply.github.com>
2020-05-26 23:24:54 -0700

    Initial commit

---
+++ force-app/main/default/aura/trafficCounterMobile/trafficCounterMobileController.js
@@ -0,0 +1,57 @@
+({
+    lookupSearch : function(component, event, helper) {
+        // Get the lookup component that fired the search event
+        const lookupComponent = event.getSource();
+        // Get the SampleLookupController.search server side action
+        const serverSearchAction = component.get('c.search');
+        // You can pass optional parameters to the search action
+        // but you can only use setParam and not setParams to do so
+        serverSearchAction.setParam('anOptionalParam', 'not used');
+        // Pass the action to the lookup component by calling the search method
+        lookupComponent.search(serverSearchAction);
+    },
+
+    clearErrorsOnChange: function(component, event, helper) {
+        const selection = component.get('v.selection');
+        const errors = component.get('v.errors');
+        if (selection.length == 0) {
+            console.log("Clear");
+            component.set("v.currentTraffic", 0);
+            component.set("v.maxTraffic", 5);
+            component.set("v.capacity", 0);
+        }
+
+        if (selection.length && errors.length) {
+            component.set('v.errors', []);
+        }
+        helper.getStoreRecord(component);
+    },
+
+    clearSelection: function(component, event, helper) {
+        component.set('v.selection', []);
+    },
+
+    increment: function(component, event, helper) {
+        var current = component.get("v.currentTraffic");
+        var max = component.get("v.maxTraffic");
+        current++;
+        console.log(current);
+        component.set("v.currentTraffic", current);
+        var cap = Math.floor( (current / max)  * 100);
+        component.set("v.capacity", cap);
+        console.log(cap);
+        helper.changeTraffic(component,"Increment");
+    },
+
+    decrement: function(component, event, helper) {
+        var current = component.get("v.currentTraffic");
+        var max = component.get("v.maxTraffic");
+        current--;
+        console.log(current)
+        component.set("v.currentTraffic", current);
+        var cap = Math.floor( (current / max)  * 100);
+        component.set("v.capacity", cap);
+        console.log(cap);
+        helper.changeTraffic(component,"Decrement");
+    }
+})
---
+++ force-app/main/default/aura/trafficCounterMobile/trafficCounterMobileHelper.js
@@ -0,0 +1,37 @@
+({
+    getStoreRecord : function(component, event) {
+        const selection = component.get('v.selection');
+        console.log(selection[0].id);
+        if (selection.length == 1) {
+            var action = component.get("c.storeDetail");
+            action.setParams({
+                storeId: selection[0].id,
+            });
+            action.setCallback(this, function(response) {
+                var store = response.getReturnValue();
+                console.log(store);
+                component.set("v.currentTraffic", store.Current_Traffic__c);
+                component.set("v.maxTraffic", store.Max_Traffic__c);
+                var cap = Math.floor( (store.Current_Traffic__c / store.Max_Traffic__c)  * 100);
+                component.set("v.capacity", cap);
+            })
+            $A.enqueueAction(action);
+
+        } 
+    },
+
+    changeTraffic : function(component, dir) {
+        const selection = component.get("v.selection");
+        console.log(selection[0].id);
+        var action = component.get("c.changeTraffic");
+        action.setParams({
+            storeId: selection[0].id,
+            changeDir: dir
+        });
+        action.setCallback(this, function(response) {
+            var store = response.getReturnValue();
+            console.log(store);
+        })
+        $A.enqueueAction(action);
+    }
+})
